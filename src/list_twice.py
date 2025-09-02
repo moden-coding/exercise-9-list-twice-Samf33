b64d4988232503fb783bdee7b0caa7b386590363
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-09-02 19:13:10 -0400

    done

--- src/list_twice.py
+++ src/list_twice.py
@@ -1,3 +1,13 @@
 # Write your solution here
 list = []
 
+while True:
+    user = int(input("New item: "))
+    if user == 0:
+        print("Bye!")
+        break
+    list.append(user)
+    print(f"The list now: {list}")
+    print(f"The list in order: {sorted(list)}")
+    
+
